3ecf02bf6c60dc974c1d2944987db471a5f3d8ca
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-04-26 11:46:36 +1000

    Started level design
    Also added some networking scripts

---
+++ Assets/Mechsplosion/Scripts/Networking/MechsplosionNetworkManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+using Mirror;
+
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Mechsplosion.Networking
+{
+    public class MechsplosionNetworkManager : NetworkManager
+    {
+        
+    }
+}
