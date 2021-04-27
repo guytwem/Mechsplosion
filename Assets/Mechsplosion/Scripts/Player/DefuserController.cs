f6f0566dbe65e58dc34ea054cffb9f95cb135d1c
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-04-27 12:41:49 +1000

    Started Level Design

---
+++ Assets/Mechsplosion/Scripts/Player/DefuserController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefuserController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10.0f;
+    [SerializeField]
+    private float jumpForce = 10.0f;
+    private Rigidbody defuserRigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defuserRigidbody = GetComponentInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        defuserRigidbody.AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    private void Jump()
+    {
+        defuserRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+}
