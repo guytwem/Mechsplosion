766057b7374e302ee8a31cdf90d038c9177a504b
Greengreener <>
2021-05-10 12:48:13 +1000

    Turrets

--- Assets/Mechsplosion/Scripts/Player/LevelController.cs
+++ Assets/Mechsplosion/Scripts/Player/LevelController.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse is down");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            RaycastHit hitInfo = new RaycastHit();
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hitInfo);
+            if (hit)
+            {
+                Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+                if (hitInfo.transform.gameObject.tag == "Turret")
+                {
+                    Debug.Log("Hit Turret");
+                    gameObject.transform.Rotate(new Vector3(0, -Input.GetAxis("Mouse X"), 0));
+                }
+                else
+                {
+                    Debug.Log("Nothing Hit");
+                }
+            }
+        }
     }
 }
