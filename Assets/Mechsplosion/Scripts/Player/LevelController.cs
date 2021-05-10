63be06ea03343272d7d6444910b5ab06c50b084f
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-05-10 13:36:34 +1000

    Implementing clicking on stuff
    raycasts why do you do this

--- Assets/Mechsplosion/Scripts/Player/LevelController.cs
+++ Assets/Mechsplosion/Scripts/Player/LevelController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+    private LevelInteractable currentInteractable;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +15,41 @@ public class LevelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit mouseClick = GetMouseClick();
+            if (mouseClick.point != Vector3.zero)
+            {
+                currentInteractable = mouseClick.collider.transform.gameObject.GetComponent<LevelInteractable>();
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if(currentInteractable != null)
+            {
+                currentInteractable.UseInteractable(GetMouseClick().point + Vector3.up);
+            }
+        }
     }
 
-    private Vector3 GetMousePosition()
+    private RaycastHit GetMouseClick()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        return Physics.RaycastAll(ray)[0].point;
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+        string hitNames = "";
+        foreach(RaycastHit hit in hits)
+        {
+            hitNames += hit.transform.gameObject.name + ", ";
+        }
+        Debug.Log(hitNames);
+        if(hits.Length > 0)
+        {
+            Debug.Log(hits[hits.Length - 1].transform.gameObject.name);
+            return hits[hits.Length - 1];
+        }
+        else
+        {
+            return default;
+        }
     }
 }
--- Assets/Mechsplosion/Scripts/Player/LevelInteractable.cs
+++ Assets/Mechsplosion/Scripts/Player/LevelInteractable.cs
@@ -2,21 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class LevelInteractable : MonoBehaviour
+public class LevelInteractable : MonoBehaviour
 {
     protected float moveSpeed = 1.0f;
 
+    /// <summary>
+    /// The level controller calls this function to use this interactable
+    /// </summary>
+    /// <param name="_location">The target location for the action</param>
     public void UseInteractable(Vector3 _location)
     {
+        StopCoroutine(nameof(Interaction));
         StartCoroutine(nameof(Interaction), _location);
     }
 
     protected virtual IEnumerator Interaction(Vector3 _location)
     {
         float lerpVal = 0;
-        while ((transform.position - _location).magnitude < 0.1)
+        Vector3 oldPos = transform.position;
+        float distance = (oldPos - _location).magnitude;
+        while ((transform.position - _location).magnitude / distance > 0.01f)
         {
-            transform.position = Vector3.Lerp(transform.position, _location, lerpVal);
+            transform.position = Vector3.Lerp(oldPos, _location, lerpVal);
             lerpVal += moveSpeed * Time.deltaTime;
             yield return null;
         }
