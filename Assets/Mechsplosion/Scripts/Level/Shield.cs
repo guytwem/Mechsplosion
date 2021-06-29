1606151efe5e1cd4c59ff52e98f27cbb2a122138
JoshMas <49138669+JoshMas@users.noreply.github.com>
2021-06-29 20:30:14 +1000

    Tutorial & crystal health indicator added

--- Assets/Mechsplosion/Scripts/Level/Shield.cs
+++ Assets/Mechsplosion/Scripts/Level/Shield.cs
@@ -7,12 +7,21 @@ using UnityEngine;
 /// </summary>
 public class Shield : MonoBehaviour
 {
-    public float health = 100.0f;
+    public float health = 10.0f;
+    private float originalHealth;
+    [SerializeField] private MeshRenderer crystal;
+    private Color originalColor;
+
+    private void Start()
+    {
+        originalColor = crystal.material.color;
+        originalHealth = health;
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
         health -= collision.rigidbody.velocity.magnitude;
-        Debug.Log(health);
+        crystal.material.color = Color.Lerp(Color.red, originalColor, health / originalHealth);
         if (health < 0)
             Destroy(gameObject);
     }
